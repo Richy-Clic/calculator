0b4fdaba216c047ed938e44b2b424568908a4819
Ricardo De anda <richy.deanda@gmail.com>
2022-10-20 22:12:43 -0500

    commit test

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ function ButtonCalcu(props) {
     <div className={`btn-calculator column-${props.cols}`} onClick={props.onClick}>
         {props.tag}
     </div>
-  )
+  )   
 }
 
 export default ButtonCalcu
